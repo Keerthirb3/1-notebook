ff63a79880efea4830f48071c03198fa262ffb36
Keerthirb3 <134645025+Keerthirb3@users.noreply.github.com>
2023-05-25 16:44:02 -0400

    Add files via upload

---
+++ 1-notebook.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# 
+# - List popular languages that Data Scientists use.
+# - List commonly used libraries used by Data Scientists.
+# - Comment on Data Science tools.
+
+# Some of the popular languages that Data Scientists use are:
+# 
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Java
+# 5. Julia
+# 6. Scala
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. TensorFlow
+# 2. NumPy
+# 3. SciPy
+# 4. Pandas
+# 5. Matplotlib
+# 6. Keras
+# 7. SciKit-Learn
+# 8. PyTorch
+# 9. Scrapy
+
+# | Data science tools |
+# | -------------------|
+# | SAS |
+# | Apache Spark |
+# | Tensor Flow |
+# | BigML |
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# 
+# 1. 3+5=8
+# 2. 4*3=12
+# 3. 14/2=7
+# 4. 5-5=0
+
+# In[1]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+
+(3*4)+5
+
+
+# In[2]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+days = 0
+hours = 0
+mins = 0
+
+time = 200
+#days = time / 1440
+leftover_minutes = time % 1440
+hours = leftover_minutes / 60
+#mins = time - (days*1440) - (hours*60)
+print(str(days) + " days, " + str(hours) + " hours, " + str(mins) +  " mins. ")
+
+
+# ## Keerthivasan Ramprabu
+
+# In[ ]:
+
+
+
+
